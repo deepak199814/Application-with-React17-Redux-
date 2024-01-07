1512c12894f106eadba31befbb5c2d00d0f58161
Kumar <deepak.kumar2@intelsat.com>
2024-01-07 19:49:48 +0530

    Add form and bind this event

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 class CoursePage extends React.Component {
+  //we can remove constructor and super(props) too but still this clas field
+  state = {
+    course: {
+      title: "",
+    },
+  };
+  handleChange = (event) => {
+    //Arrow function will inherit the binding context of their enclosing scope[class fields]
+    const course = { ...this.state.course, title: event.target.value };
+    //this.setState({ course: course });
+    this.setState({ course });
+  };
   render() {
-    return <h2>Couerses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 export default CoursePage;
